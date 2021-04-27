c36ff5dbb9b63ae90f8eb5fe7b27a386b2c91964
Marta Kolakowska <kolakowska.ma@gmail.com>
2021-04-27 21:31:33 +0200

    Algorytm init commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printHelloWorld();
+        giveValue(1);
+        int resultSum = sumNumber(14, 105);
+        System.out.println(resultSum);
+
+        int result = substraction(13, 17);
+        System.out.println(result);
+
+        int resultMultiplication = multiplication(resultSum, result);
+        System.out.println(resultMultiplication);
+
+        boolean even = checkEvenNumber(12);
+        System.out.println("Even result="+even );
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Podaj imię");
+        String name = scanner.nextLine();
+        System.out.println("Witaj. Twoje imię to " + name);
+
+
+        // write your code here
+    }
+
+    private static boolean checkEvenNumber(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    private static int multiplication(int resultSum, int result) {
+        return resultSum * result;
+    }
+
+    private static int substraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    private static int sumNumber(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    private static void giveValue(int number) {
+        if (number > 5) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+
+        }
+    }
+
+    private static void printHelloWorld() {
+        String name = "Hello World";
+        System.out.println(name);
+    }
+
+}
