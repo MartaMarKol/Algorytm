7d15ab7eb3e6539b4022d15a504ff9c62d770477
Marta Kolakowska <kolakowska.ma@gmail.com>
2021-04-27 21:51:22 +0200

    Change sumNumber parameters.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         printHelloWorld();
         giveValue(1);
-        int resultSum = sumNumber(14, 105);
+        int resultSum = sumNumber(15, 10);
         System.out.println(resultSum);
 
         int result = substraction(13, 17);
